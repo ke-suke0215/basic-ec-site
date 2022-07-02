2c70a5e52df8ebb5065c13db51b7e21f78c273eb
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-07-02 14:17:31 +0900

    first commit

---
+++ src/main/java/com/example/BasicEcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicEcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicEcSiteApplication.class, args);
+	}
+
+}
