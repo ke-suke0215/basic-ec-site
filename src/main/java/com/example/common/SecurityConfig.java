99661bb9c17a30d1748a398ef053acfea5cdc1fe
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-07-03 13:17:43 +0900

    ログイン認証フィルター実装

--- src/main/java/com/example/common/SecurityConfig.java
+++ src/main/java/com/example/common/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.common;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @SuppressWarnings("deprecation")
@@ -61,5 +64,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	}
 	
+	/**
+     * <pre>
+     * bcryptアルゴリズムでハッシュ化する実装を返します.
+     * これを指定することでパスワードハッシュ化やマッチ確認する際に
+     * @Autowired
+	 * private PasswordEncoder passwordEncoder;
+	 * と記載するとDIされるようになります。
+     * </pre>
+     * @return bcryptアルゴリズムでハッシュ化する実装オブジェクト
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    		return new BCryptPasswordEncoder();
+    }
 	
 }
