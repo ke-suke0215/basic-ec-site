49b6e7f01f80c0e4cbec568b6f8bf8db7f44205c
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-07-12 07:41:57 +0900

    何してたか忘れたけど、とりあえず日が空いちゃったからコミット

--- src/main/java/com/example/common/SecurityConfig.java
+++ src/main/java/com/example/common/SecurityConfig.java
@@ -26,12 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
-			.antMatchers( "/css/**"
-						, "/img/**"
-						, "/js/**"
-						, "/fonts/**");
+				.antMatchers("/css/**", "/img/**", "/js/**", "/fonts/**");
 	}
-	
+
 	/**
 	 * このメソッドをオーバーライドすることで、認可の設定やログイン/ログアウトに関する設定ができる.
 	 * 
@@ -41,42 +38,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests() // 認可に関する設定
-			.antMatchers("/", "/user/**").permitAll() //「/」などのパスは全てのユーザに許可
-			//.antMatchers("/admin/**").hasRole("ADMIN") // /admin/から始まるパスはADMIN権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
-			//.antMatchers("/user/**").hasRole("USER") // /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
-			.anyRequest().authenticated(); // それ以外のパスは認証が必要
+				.antMatchers("/", "/user/**").permitAll() // 「/」などのパスは全てのユーザに許可
+				// .antMatchers("/admin/**").hasRole("ADMIN") //
+				// /admin/から始まるパスはADMIN権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
+				// .antMatchers("/user/**").hasRole("USER") //
+				// /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
+				.anyRequest().authenticated(); // それ以外のパスは認証が必要
 
 		http.formLogin() // ログインに関する設定
-			.loginPage("/user/toLogin") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
-			.loginProcessingUrl("/user/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
-			.failureUrl("/user/toLogin?error=true") //ログイン失敗に遷移させるパス
-			.defaultSuccessUrl("/", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
-			                                        // 第2引数: true :認証後常に第1引数のパスに遷移 
-			                                        //         false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
-			.usernameParameter("email") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
-			.passwordParameter("password"); // 認証時に使用するパスワードのリクエストパラメータ名
-		
+				.loginPage("/user/toLogin") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
+				.loginProcessingUrl("/user/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
+				.failureUrl("/user/toLogin?error=true") // ログイン失敗に遷移させるパス
+				.defaultSuccessUrl("/", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
+																				// 第2引数: true :認証後常に第1引数のパスに遷移
+																				// false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
+				.usernameParameter("email") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
+				.passwordParameter("password"); // 認証時に使用するパスワードのリクエストパラメータ名
+
 		http.logout() // ログアウトに関する設定
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout**")) // ログアウトさせる際に遷移させるパス
-			.logoutSuccessUrl("/user/toLogin") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
-			.deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
-			.invalidateHttpSession(true); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
-		
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout**")) // ログアウトさせる際に遷移させるパス
+				.logoutSuccessUrl("/user/toLogin") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
+				.deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
+				.invalidateHttpSession(true); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
+
 	}
-	
+
 	/**
-     * <pre>
-     * bcryptアルゴリズムでハッシュ化する実装を返します.
-     * これを指定することでパスワードハッシュ化やマッチ確認する際に
-     * @Autowired
-	 * private PasswordEncoder passwordEncoder;
-	 * と記載するとDIされるようになります。
-     * </pre>
-     * @return bcryptアルゴリズムでハッシュ化する実装オブジェクト
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-    		return new BCryptPasswordEncoder();
-    }
-	
+	 * <pre>
+	 * bcryptアルゴリズムでハッシュ化する実装を返します.
+	 * これを指定することでパスワードハッシュ化やマッチ確認する際に
+	 * @Autowired
+	* private PasswordEncoder passwordEncoder;
+	* と記載するとDIされるようになります。
+	 * </pre>
+	 * 
+	 * @return bcryptアルゴリズムでハッシュ化する実装オブジェクト
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
